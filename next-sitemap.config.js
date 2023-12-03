bf5454b6c4127af022a8dd0d512774b45e7b4930
Multimedia <alizulfikar032@gmail.com>
2023-12-03 20:53:03 +0700

    sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://bromo-sunrise-vinny.vercel.app/',
+    generateRobotsTxt: true,
+
+}
