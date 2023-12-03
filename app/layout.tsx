1f8ee0a4e8e3507a484988d268adccc82701d46e
Multimedia <alizulfikar032@gmail.com>
2023-12-03 20:45:12 +0700

    optimze SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,59 @@
-"use client"
-
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import Header from "@/components/partials/header";
 import React from "react";
 import FooterPartials from "@/components/partials/footer";
-import {CustomFlowbiteTheme, Flowbite} from "flowbite-react";
-import customTheme from "@/components/utils/theme";
+
 const inter = Inter({subsets: ['latin']})
 
 
-
-const metadata: Metadata = {
+export const metadata: Metadata = {
     title: 'Vinny Bromo Sunrise Tour',
     description: 'Vinny Bromo Sunrise Tour is a tour and travel agency in Malang, East Java, Indonesia.',
+    authors: {
+        name: 'Muhammad Ali Zulfikar',
+        url: 'https://www.alizul01.my.id',
+    },
+    abstract: 'Vinny Bromo Sunrise Tour is a tour and travel agency in Malang, East Java, Indonesia. We provide tour packages around East Java for domestic and international tourists. We also provide car rental services for those of you who want to travel in Malang and surrounding areas. We are ready to serve you 24 hours.',
+    icons: {
+        icon: '/favicon.ico',
+    },
+    keywords: [
+        'tour',
+        'travel',
+        'tour and travel',
+        'tour packages',
+        'car rental',
+        'bromo tour',
+        'bromo sunrise tour',
+        'bromo midnight tour',
+        'bromo ijen tour',
+        'bromo malang tour',
+        'bromo malang midnight tour',
+        'bromo malang ijen tour',
+        'bromo malang midnight ijen tour',
+        'malang tour',
+        'malang midnight tour',
+    ],
+    robots: {
+        follow: true,
+        index: true,
+    },
+    category: 'Tour and Travel',
+    referrer: 'no-referrer-when-downgrade',
+    twitter: {
+        images: [
+            {
+                url: '/assets/image/products/bromo/1.png',
+                alt: 'Bromo Tour',
+                type: 'image/png',
+                height: 630,
+            },
+        ],
+    },
+    applicationName: 'Vinny Bromo Sunrise Tour',
+    creator: 'Vinny Bromo Sunrise Tour',
 }
 
 export default function RootLayout({
@@ -25,13 +64,11 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        <Flowbite theme={{theme: customTheme}}>
-            <div className={'z-30 absolute w-full'}>
-                <Header />
-            </div>
-            {children}
-            <FooterPartials />
-        </Flowbite>
+        <div className={'z-30 absolute w-full'}>
+            <Header/>
+        </div>
+        {children}
+        <FooterPartials/>
         </body>
         </html>
     )
--- components/component/card-features.tsx
+++ components/component/card-features.tsx
@@ -34,59 +34,61 @@ const CardFeatures: React.FC<CardFeaturesProps> = ({ title, slug, description, p
     };
 
     return (
-        <div className={'flex w-full gap-16 rounded-lg p-2 items-center' + (isEven ? ' flex-row' : ' flex-row-reverse')}>
-            <div className="relative w-[72rem] h-[26rem] rounded-lg">
+        <div className={`flex w-full gap-4 lg:gap-16 rounded-xl p-2 items-center flex-col justify-center bg-white px-4 md:px-8 lg:px-12 ${isEven ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:py-8 hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-103 hover:bg-gray-50 outline-none`}>
+            <div className="relative w-full h-80 md:h-96 lg:w-[54rem] lg:h-[26rem] rounded-lg max-w-3xl">
                 <Image
                     src={`/assets/image/products/${image}/1.png`}
                     alt={title}
-                    layout="fill"
-                    objectFit="cover"
-                    className={'rounded-lg mr-4'}
+                    fill
+                    sizes={'100%'}
+                    style={{ objectFit: "cover" }}
+                    className={'rounded-lg'}
                 />
             </div>
-            <div className={'flex flex-col items-start w-full py-8 gap-2'}>
-                <h2 className={'text-xl xl:text-3xl font-bold text-gray-900'}>
+            <div className={'flex flex-col items-start w-full lg:py-8 gap-2'}>
+                <h2 className={'text-xl md:text-3xl lg:text-4xl font-bold text-gray-900'}>
                     {title}
                 </h2>
-                <p className={'text-gray-500 font-light text-lg text-start tracking-tight leading-8'}>
+                <p className={'text-gray-500 font-light text-lg md:text-base text-start tracking-tight leading-8'}>
                     {description}
                 </p>
-                <div className={'flex flex-row gap-4 mt-4'}>
+                <div className={'flex flex-row gap-2 md:gap-4 mt-4'}>
                     <IconText icon={<BiSolidUser className={'text-2xl text-orange-400'} />} text={'Private Tour'} />
                     <IconText icon={<BiSolidGroup className={'text-2xl text-orange-400'} />} text={'Group Tour'} />
                 </div>
-                <div className={'container w-full h-0.5 bg-gray-200 rounded-full'}/>
-                <div className={'flex flex-row gap-4 justify-around w-full items-start'}>
+                <div className={'container w-full h-0.5 bg-gray-200 rounded-full mt-4 md:mt-6'}/>
+                <div className={'flex flex-col md:flex-row gap-4 justify-around w-full items-start'}>
                     {Array.from({ length: 3 }, (_, index) => (
-                        <div key={index} className="relative w-40 h-40">
+                        <div key={index} className="relative w-full md:w-56 h-40 md:h-56">
                             <Image
                                 src={`/assets/image/products/${image}/${index + 2}.png`}
                                 alt={title}
-                                layout="fill"
-                                objectFit="cover"
+                                fill
+                                sizes={'100%'}
+                                style={{ objectFit: "cover" }}
                                 className={'rounded-lg'}
                             />
                         </div>
                     ))}
                 </div>
-                <div className={'container w-full h-0.5 bg-gray-200 rounded-full'}/>
-                <div className={'flex flex-row justify-between w-full items-center'}>
-                    <div className={'flex flex-col gap-2'}>
+                <div className={'container w-full h-0.5 bg-gray-200 rounded-full mt-4 md:mt-6'}/>
+                <div className={'flex flex-col md:flex-row justify-between w-full items-center'}>
+                    <div className={'flex flex-row gap-2'}>
                         <div>
-                            <p className={'text-gray-500 font-light text-lg text-start tracking-tight leading-8'}>
+                            <p className={'text-gray-500 font-light text-lg md:text-base text-start tracking-tight leading-8'}>
                                 Start from
                             </p>
                         </div>
                         <div>
-                            <p className={'text-gray-900 font-medium text-lg text-start tracking-tight leading-8'}>
+                            <p className={'text-gray-900 font-medium text-xl md:text-2xl text-start tracking-tight leading-8'}>
                                 {formatToRupiah(lowestDomesticPrice)}
                             </p>
                         </div>
                     </div>
-                    <div className={'flex flex-row gap-4'}>
-                        <Link className={'flex flex-row gap-2 items-center'} href={`/tour/${slug}`}>
+                    <div className={'flex flex-row gap-2 md:gap-4 mt-4 md:mt-0 w-full lg:w-fit'}>
+                        <Link className={'bg-orange-500 p-2 px-4 rounded-lg text-white flex flex-row justify-center gap-2 items-center w-full'} href={`/tour/${slug}`}>
                             <div>
-                                <p className={'text-gray-500 font-light text-lg text-start tracking-tight leading-8'}>
+                                <p className={'text-white font-medium text-lg md:text-base tracking-tight leading-8 text-center'}>
                                     See more
                                 </p>
                             </div>
