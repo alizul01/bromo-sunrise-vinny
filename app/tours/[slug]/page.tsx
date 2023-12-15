ed4bcdcec249e5771b81f4ed98c8a37db4b75f18
Multimedia <alizulfikar032@gmail.com>
2023-12-15 07:56:14 +0700

    add: tour detail page

--- app/tours/[slug]/page.tsx
+++ app/tours/[slug]/page.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+"use client"
 
-const TourDetail: React.FC = ({tour} : any) => {
+import React from "react";
+import {usePathname} from "next/navigation";
+import {TOUR_DATA} from "@/data/tour_data";
+import Hero from "@/components/pages/tour_detail/hero";
+import Title from "@/components/pages/tour_detail/title";
+
+const TourDetail: React.FC = () => {
+    const router = usePathname();
+    const currentSlug = router.split('/')[2];
+
+    const tour = TOUR_DATA.find(tour => tour.slug === currentSlug);
+    if (!tour) return null;
+    const imageUrl = `/assets/image/products/${tour.image}/1.png`;
+
     return (
         <div>
-            <h1>
-
-            </h1>
+            <Hero imageUrl={imageUrl}/>
+            <div className={'px-6 xl:px-0 container mx-auto mt-10 start py-4 mb-12 flex gap-8'}>
+                <div className={''}>
+                    <Title title={tour.title} subtitle={tour.description} />
+                </div>
+                <div className={'w-1/4'}>
+                    Test
+                </div>
+            </div>
         </div>
     )
 }
